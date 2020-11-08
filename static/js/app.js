3934a02757790daed2ec9f1e355a062f9b04816c
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-11-08 18:09:44 -0500

    main

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,11 @@
-// Using d3 library to read samples
-d3.json("/samples.json", function(data) {
-    console.log(data);
+// Using d3 library to read samples json
+function getOtu() {
+    d3.json("/samples.json", function(data) {
+        // console.log(data);
+        var sample_values = unpack(data.samples.sample_values);
+        var otu_ids = unpack(data.samples.otu_ids);
+        var otu_labels = unpack(data.samples.otu_labels);
+        console.log(sample_values);
 });
+}
+//Build horizintal bar for top 10 OTUs in each individual
