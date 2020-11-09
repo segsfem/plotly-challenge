a91d891e5f7fc8904c5ca5fa3af04a8e0327d130
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-11-08 21:41:48 -0500

    main

--- static/js/app.js
+++ static/js/app.js
@@ -9,3 +9,26 @@ function getOtu() {
 });
 }
 //Build horizintal bar for top 10 OTUs in each individual
+
+var trace1 = {
+    type: "bar",
+    name: otu_labels,
+    x: otu_ids,
+    y: sample_values
+};
+
+var data = [trace1];
+var layout = {
+    title: `Belly Button Biodiversity OTUs`,
+    xaxis: {
+        range: [otu_ids],
+        type: "name"
+    },
+    yaxis: {
+        type: "bar"
+    },
+    showlegend: false
+
+};
+Plotly.newPlot("plot", data, layout);
+getOtu();
