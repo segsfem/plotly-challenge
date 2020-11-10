4b2790066447f938f1086cbb54e25552f0a9daf4
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-11-09 23:29:29 -0500

    main

--- static/js/app.js
+++ static/js/app.js
@@ -4,8 +4,18 @@ function unpack(rows, index) {
         return row[index];
       });
 }
+
+// Submit Button handler
+function handleSubmit() {
+    // Prevent the page from refreshing
+    Plotly.d3.event.preventDefault();
+
+    // Select the input value from the form
+  var member_id = Plotly.d3.select("#selDataset").node().value;
+
 function getOtu() {
-    d3.json("data/samples.json", function(data) {
+    var queryJson = `data/samples.json`
+    d3.json(queryJson).then(function(data) {
         // console.log(data);
         var sample_values = unpack(data.samples.sample_values);
         var otu_ids = unpack(data.samples.otu_ids);
